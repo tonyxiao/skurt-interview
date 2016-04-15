43731929746ec2b12a795f9c1d1380216778c016
Tony Xiao <tonyx.ca@gmail.com>
2016-04-14 18:21:15 -0700

    Checking every min will NOT overload most APIs

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ import {checkCarsAndSendError} from './lib'
 
 const carIds = [1,2,3,4,5,6,7,8,9,10]
 
-// Native algorithm to just pull every 2 minutes. 
+// Native algorithm to just pull every minute
 // More sophisticated production system will include things like exponential backoff on 
 // failure, and TCP-style congestion control.
-const seconds = 60 * 2
+const seconds = 60
 
 // Start immediately
 checkCarsAndSendError(carIds)
 
-// They every 2 minutes thereafter
+// They every minute thereafter
 setInterval(() => {
   checkCarsAndSendError(carIds)
 }, seconds * 1000)

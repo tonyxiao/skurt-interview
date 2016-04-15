57908f5e4532718df5a6f6a960edf11b0d62f05b
Tony Xiao <tonyx.ca@gmail.com>
2016-04-14 17:22:02 -0700

    Signup for mailgun account, use mailgun to send error emails

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
-import {isCarInBound, areCarsInBound} from './lib'
+import {isCarInBound, areCarsInBound, sendErrorEmail} from './lib'
 
 async function main() {
   try {
     // const carId = 2
     // const inBounds = await isCarInBound(carId)
     // console.log(`Car number ${carId} is inBounds=${inBounds}`)
-    const results = await areCarsInBound([1,2,3,4,5,6,7,8,9,10])
-    console.log('inbounds', results)
+    // const results = await areCarsInBound([1,2,3,4,5,6,7,8,9,10])
+    // console.log('inbounds', results)
+    
+    const subject = 'Hello Tony Xiao'
+    const body = 'Congratulations Tony Xiao, you just sent an email with Mailgun!  You are truly awesome!  You can see a record of this email in your logs: https://mailgun.com/cp/log .  You can send up to 300 emails/day from this sandbox server.  Next, you should add your own domain so you can send 10,000 emails/month for free.'
+    const res = await sendErrorEmail(subject, body)
+    console.log('res', res)
   } catch (err) {
     console.log(err)
   }
 }
+
+
+
 
 main()
--- lib.js
+++ lib.js
@@ -1,5 +1,6 @@
 import request from 'request'
 import gju from 'geojson-utils'
+import mailgun from 'mailgun-js'
 
 async function getJson(url) {
   return new Promise((resolve, reject) => {
@@ -28,4 +29,18 @@ export async function areCarsInBound(carIds) {
     resultsByCarId[carId] = results[index]
   })
   return resultsByCarId
+}
+
+export async function sendErrorEmail(subject, text) {
+  // XXX: DO NOT PUT API KEY IN SRC CODE IN PROD
+  const apiKey = 'key-0581405a6a7cc054db09a93141ad6544'
+  const domain = 'sandboxd1bddbf67c514f94a6849ed21a670f15.mailgun.org'
+  const mailer = mailgun({apiKey, domain})
+  const data = {
+    from: 'Error Reporter <postmaster@sandboxd1bddbf67c514f94a6849ed21a670f15.mailgun.org>',
+    to: 'tonyx.ca@gmail.com',
+    subject,
+    text,
+  }
+  return await mailer.messages().send(data)
 }
